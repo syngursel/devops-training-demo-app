fb99c5105d7f99cc81df839fe38b38be4d312f5d
gurselgazii <gursel.ictuzer@teamdefinex.com>
2022-08-23 16:31:34 +0300

    unittest

--- src/test/java/com/definex/devopstraining/DevopsTrainingApplicationTests.java
+++ src/test/java/com/definex/devopstraining/DevopsTrainingApplicationTests.java
@@ -1,13 +1,35 @@
 package com.definex.devopstraining;
 
+import com.definex.devopstraining.data.dao.BookRepository;
+import com.definex.devopstraining.data.model.Book;
+import com.definex.devopstraining.service.BookService;
+import com.definex.devopstraining.service.model.BookDto;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 @SpringBootTest
 class DevopsTrainingApplicationTests {
 
+    @Autowired
+    private BookService bookService;
+
+    @MockBean
+    private BookRepository bookRepository;
+
     @Test
-    void contextLoads() {
+    public void getBookTest() {
+        Mockito.when(bookRepository.findAll())
+                .thenReturn(List.of(new Book()));
+
+        final List<BookDto> dtoList = bookService.findAll();
+        Assertions.assertEquals(1, dtoList.size());
     }
 
 }
